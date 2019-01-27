648e73384c5d7765ca220b1a526ffab6c9f062d3
kf <k.falinski@sdacademy.pl>
2019-01-27 14:54:08 +0100

    While Do while

--- src/main/java/pl/sda/javastart/day2/ForsExample.java
+++ src/main/java/pl/sda/javastart/day2/ForsExample.java
@@ -9,7 +9,13 @@ public class ForsExample {
         printFewNaturalNumbers(5);
         printFewNumbersWithoutDividableByParam(30, 3);
         System.out.println(" Metoda z continue: ");
-        printFewNumbersWithoutDividableByParamContinue(10,2);
+        printFewNumbersWithoutDividableByParamContinue(10, 2);
+        System.out.println(sumNaturalNumbersToLimit(1000));
+        System.out.println(" Piramida ");
+        printNumbersInTower(9);
+        System.out.println(sumWhile(1000));
+        System.out.println(sumDoWhile(0));
+        fibonacci(6);
     }
 
     public static void printFewNaturalNumbers(int max) {
@@ -35,4 +41,57 @@ public class ForsExample {
         }
     }
 
+    public static int sumNaturalNumbersToLimit(int max) {
+        int sum = 0;
+        for (int i = 0; ; i++) {
+            if (sum + i >= max) {
+                break;
+            }
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static void printNumbersInTower(int param) {
+        for (int i = 1; i <= param; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int sumWhile(int max) {
+        int sum = 0;
+        int counter = 0;
+        while (sum+counter <= max) {
+            sum = sum + (++counter);
+        }
+        return sum;
+    }
+
+    public static int sumDoWhile (int max){
+        int sum = 0;
+        int counter = 0;
+        do {
+            sum=sum+ (++counter);
+        } while (sum+counter<=max);
+        return sum;
+    }
+
+    public static void fibonacci (int param){
+        System.out.println("fibonacci ");
+        int first = 0;
+        int second = 1;
+        int counter = 1;
+        System.out.println("0");
+        System.out.println("1");
+        while(counter<=param-2){
+        int sum = first + second;
+            System.out.println(sum);
+            first=second;
+            second=sum;
+            counter++;
+        }
+    }
 }
